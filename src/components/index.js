55c3b1feee26bd3655b836c5554a336bc80c9398
Reza Firdaus <reza.firdaus@gramedia.id>
2024-02-28 22:05:41 +0700

    feat: WIP homepage

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Navbar from "./navbar/Navbar.jsx";
+import Footer from "./footer/Footer.jsx";
+
+export { Navbar, Footer };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./routes/error-page.jsx";
+import Homepage from "./routes/homepage.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				index: true,
+				element: <Homepage />,
+			},
+		],
+	},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
